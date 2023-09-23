13020aca6b350fd1068926432cd04312bb601c74
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-09-23 20:08:40 +0200

    data validation tests

--- tests/data_validation/test_data_prep.py
+++ tests/data_validation/test_data_prep.py
@@ -1,7 +1,45 @@
 import os
-import papermill as pm
+import pytest
+from recommenders.datasets import movielens
 
 
-def test_data_prep_runs(path_notebooks, output_notebook, kernel_name):
-    notebook_path = os.path.join(path_notebooks, "01_data_prep.ipynb")
-    pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name)
+@pytest.fixture(scope="function")
+def movielens_cols():
+    return {
+        "size": "100k",
+        "header": ["user_id", "item_id", "rating", "timestamp"],
+        "title_col": "title",
+        "genres_col": "genres",
+        "year_col": "year",
+    }
+
+
+@pytest.fixture(scope="function")
+def movielens_100k_first_row():
+    return {
+        "user_id": 196,
+        "item_id": 1,
+        "rating": 3,
+        "timestamp": 881250949,
+        "title": "Toy Story (1995)",
+        "genres": "Animation|Children's|Comedy",
+        "year": "1995",
+    }
+
+
+def test_movielens(movielens_cols, movielens_100k_first_row):
+    df = movielens.load_pandas_df(**movielens_cols)
+    assert len(df) == 100000
+    assert df.columns.tolist() == [
+        "user_id",
+        "item_id",
+        "rating",
+        "timestamp",
+        "title",
+        "genres",
+        "year",
+    ]
+    assert df.iloc[0]["user_id"] == movielens_100k_first_row["user_id"]
+    # assert movielens_100k_first_row["item_id"] == 242
+    # assert movielens_100k_first_row["rating"] == 3
+    # assert movielens_100k_first_row["timestamp"] == 881250949
