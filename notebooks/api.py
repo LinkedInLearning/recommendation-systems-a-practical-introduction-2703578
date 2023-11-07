0d46a25020112933d0096431c167a8691070f39b
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-11-07 23:06:38 +0100

    Modified api with flask

--- notebooks/api.py
+++ notebooks/api.py
@@ -1,34 +1,53 @@
-import cherrypy
+from flask import Flask, request, jsonify
 import psycopg2
 
 # NOTE: MODIFY THE FILE secrets.template.py
 from reco_secrets import DATABASE, USER, PASS
 
 
-class UserItemsAPI:
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def get_items(self, userID):
-        conn = psycopg2.connect(
-            database=DATABASE,
-            user=USER,
-            password=PASS,
-            host="localhost",
-            port="5432",
-        )
-        cur = conn.cursor()
-
-        select_query = "SELECT itemID FROM user_items WHERE userID = %s;"
-        cur.execute(select_query, (userID,))
-        result = cur.fetchall()
-
-        conn.close()
-
-        # Convert the result to a list of dictionaries
-        item_ids = [row[0] for row in result]
-
-        return {"itemIDs": item_ids}
+app = Flask(__name)
 
 
-if __name__ == "__main__":
-    cherrypy.quickstart(UserItemsAPI())
+def get_recommendations(user_id):
+    conn = psycopg2.connect(
+        database=DATABASE,
+        user=USER,
+        password=PASS,
+        host="localhost",
+        port="5432",
+    )
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM recommendations WHERE user_id = %s", (user_id,))
+    row = cur.fetchone()
+    
+    cur.close()
+    conn.close()
+    
+    if row is None:
+        return None
+    else:
+        columns = [desc[0] for desc in cur.description]
+        recommendation_data = dict(zip(columns, row))
+        del recommendation_data['user_id']  # Remove the 'user_id' from the result
+        return recommendation_data
+
+
+@app.route('/recommendations', methods=['POST'])
+def recommend_items():
+    data = request.get_json()
+
+    if data is None or 'user_id' not in data:
+        return jsonify({'error': 'Invalid request data'}), 400
+
+    user_id = data['user_id']
+    recommendations = get_recommendations(user_id)
+    
+    if recommendations is None:
+        return jsonify({'error': 'User not found'}), 404
+    else:
+        return jsonify(recommendations)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
