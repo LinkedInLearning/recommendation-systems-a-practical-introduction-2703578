27091403819c1043c35dd9e95840dc9f434b6ecd
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-12-26 08:14:45 +0100

    remove map

--- tests/functional/test_sar.py
+++ tests/functional/test_sar.py
@@ -4,7 +4,6 @@ from recommenders.datasets import movielens
 from recommenders.datasets.python_splitters import python_stratified_split
 from recommenders.models.sar import SAR
 from recommenders.evaluation.python_evaluation import (
-    map_at_k,
     ndcg_at_k,
     precision_at_k,
     recall_at_k,
@@ -29,9 +28,7 @@ def test_sar_movielens_functional(data, sar_header):
     model = SAR(**sar_header)
     model.fit(train)
     top_k = model.recommend_k_items(test, top_k=TOP_K, remove_seen=True)
-    eval_map = map_at_k(
-        test, top_k, col_user="userID", col_item="itemID", col_rating="rating", k=TOP_K
-    )
+
     eval_ndcg = ndcg_at_k(
         test, top_k, col_user="userID", col_item="itemID", col_rating="rating", k=TOP_K
     )
@@ -42,7 +39,6 @@ def test_sar_movielens_functional(data, sar_header):
         test, top_k, col_user="userID", col_item="itemID", col_rating="rating", k=TOP_K
     )
 
-    assert eval_map == pytest.approx(0.106959, abs=ABS_TOL)
     assert eval_ndcg == pytest.approx(0.379533, abs=ABS_TOL)
     assert eval_precision == pytest.approx(0.331071, abs=ABS_TOL)
     assert eval_recall == pytest.approx(0.176837, abs=ABS_TOL)
